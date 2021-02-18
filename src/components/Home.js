3cc538ded945b486e8ba575e4cae8576f73850df
Maor Lahav <maorlahav5@cs.colman.ac.il>
2021-02-18 11:14:50 +0200

    add image with explanation of the program

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,12 @@ const Home = () => {
     return (
         <div className="container">
             <h4 className="center">Home</h4>
-            <p>{`${faker.lorem.paragraph()}`}</p>
+            <p>This program intended for manage Terminal Y store.<br/>
+                You can see daily graph of Financial gains.<br/>
+                You can also manage your store, add new item and delete exist item, also you can edit stock of exist item.<br/>
+                You can in the users tag to manage users in the program and delete users.
+            </p>
+            <img height='400px' src={process.env.PUBLIC_URL +"/teminaly.png"}/>
         </div>
     )
 }
